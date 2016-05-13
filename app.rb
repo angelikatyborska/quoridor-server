5977c1b425e848ba488030456ee994456e011f23
Angelika Tyborska <atyborska93@gmail.com>
2016-05-13 18:18:33 +0200

    Keep connection alive

--- app.rb
+++ app.rb
@@ -3,9 +3,13 @@ require 'faye/websocket'
 App = lambda do |env|
   if Faye::WebSocket.websocket?(env)
     ws = Faye::WebSocket.new(env)
+    ping_loop = nil
 
     ws.on :open do |event|
       puts [:open]
+      ping_loop = EM.add_periodic_timer(3) do
+        ws.ping
+      end
     end
 
     ws.on :message do |event|
@@ -14,6 +18,8 @@ App = lambda do |env|
     end
 
     ws.on :close do |event|
+      EM.cancel_timer(ping_loop)
+
       p [:close, event.code, event.reason]
       ws = nil
     end
