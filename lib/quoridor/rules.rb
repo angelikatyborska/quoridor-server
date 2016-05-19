56438e4d08bc4cd56b59760e8a21568f88614a6f
Angelika Tyborska <atyborska93@gmail.com>
2016-05-19 20:51:37 +0200

    Make Rules#possible_moves aware of fences

--- lib/quoridor/rules.rb
+++ lib/quoridor/rules.rb
@@ -2,7 +2,7 @@ module Quoridor
   class Rules
     def possible_moves(board, pawnNumber)
       pawn = board.pawns[pawnNumber]
-      board.neighboring_squares(pawn)
+      board.neighboring_squares(pawn).reject {|square| board.fence?(square, pawn)}
     end
   end
 end
--- spec/lib/quoridor/rules_spec.rb
+++ spec/lib/quoridor/rules_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Quoridor::Rules do
           board.add_pawn('e9')
         end
 
-        it 'from the northmost row allows movement: east, south, west' do
+        it 'allows movement: east, south, west' do
           expect(rules.possible_moves(board, 0)).to contain_exactly('f9', 'e8', 'd9')
         end
       end
@@ -22,7 +22,7 @@ RSpec.describe Quoridor::Rules do
           board.add_pawn('i5')
         end
 
-        it 'from the eastmost row allows movement: north, south, west' do
+        it 'fallows movement: north, south, west' do
           expect(rules.possible_moves(board, 0)).to contain_exactly('i6', 'h5', 'i4')
         end
       end
@@ -32,13 +32,24 @@ RSpec.describe Quoridor::Rules do
           board.add_pawn('e5')
         end
 
-        it 'from the eastmost row allows movement: north, east, south, west' do
+        it 'allows movement: north, east, south, west' do
           expect(rules.possible_moves(board, 0)).to contain_exactly('e6', 'f5', 'e4', 'd5')
         end
       end
     end
 
-    context 'some fences, one pawn'
+    context 'some fences, one pawn' do
+      before(:each) do
+        board.add_pawn('e5')
+        board.add_fence('d5v')
+        board.add_fence('e5h')
+      end
+
+      it 'allows movement: north, east' do
+        expect(rules.possible_moves(board, 0)).to contain_exactly('e6', 'f5')
+      end
+    end
+
     context 'no fences, many pawns'
     context 'some fences, many pawns'
   end
