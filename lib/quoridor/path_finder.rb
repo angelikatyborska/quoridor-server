4db3310556d0c7c44ad43645f6e3c3d326445aab
Angelika Tyborska <atyborska93@gmail.com>
2016-05-26 15:31:39 +0200

    Refactor PathFinder

--- lib/quoridor/path_finder.rb
+++ lib/quoridor/path_finder.rb
@@ -6,41 +6,39 @@ module Quoridor
 
     def find(start_square, target_squares)
       open = {}
+      closed = {}
 
-      open[start_square] = { parent: :start }
-      fill_costs(start_square, target_squares, open)
-
-      current_square = pop_min(open)
-      closed = [current_square].to_h
+      fill_data(start_square, :start, target_squares, open)
+      current_square = process_square(open, closed)
 
       while current_square && !target_squares.include?(current_square[0])
-        @board.adjacent_squares(current_square[0]).each do |square|
-          unless closed[square] || open[square] || @board.fence?(square, current_square[0])
-            open[square]= { parent: current_square[0] }
-            fill_costs(square, target_squares, open)
-          end
-        end
-
-        current_square = pop_min(open)
-        closed.merge!([current_square].to_h) if current_square
+        fill_neighbors_data(current_square, target_squares, open, closed)
+        current_square = process_square(open, closed)
       end
 
-      closed_target_squares = closed.select { |square, costs| target_squares.include?(square) }
-      return nil if closed_target_squares.empty?
-
-      closest_target_costs_array = closed_target_squares.min_by { |square, costs| costs[:g] }
-
-      parent = closest_target_costs_array[0]
-      path = [parent]
-
-      while (parent = closed[parent][:parent]) && parent != :start
-        path << parent
-      end
-
-      path.reverse[1..-1]
+      collect_path(target_squares, closed)
     end
 
     private
+
+    def fill_neighbors_data(current_square, target_squares, open, closed)
+      @board.adjacent_squares(current_square[0]).each do |square|
+        unless closed[square] || open[square] || @board.fence?(square, current_square[0])
+          fill_data(square, current_square[0], target_squares, open)
+        end
+      end
+    end
+
+    def fill_data(square, parent, target_squares, open)
+      open[square]= { parent: parent }
+      fill_costs(square, target_squares, open)
+    end
+
+    def process_square(open, closed)
+      current_square = pop_min(open)
+      closed.merge!([current_square].to_h) if current_square
+      current_square
+    end
 
     def fill_costs(current_square, target_squares, costs)
       parent = costs[costs[current_square][:parent]]
@@ -48,6 +46,20 @@ module Quoridor
       costs[current_square][:h] = heuristic(target_squares, current_square)
       costs[current_square][:g] = (parent ? parent[:g] : 0) + 1
       costs[current_square][:f] = costs[current_square][:h] + costs[current_square][:g]
+    end
+
+    def collect_path(target_squares, closed)
+      closed_target_squares = closed.select { |square, costs| target_squares.include?(square) }
+      return nil if closed_target_squares.empty?
+
+      parent = closed_target_squares.min_by { |square, costs| costs[:g] }[0]
+      path = [parent]
+
+      while (parent = closed[parent][:parent]) && parent != :start
+        path << parent
+      end
+
+      path.reverse[1..-1]
     end
 
     def pop_min(open)
