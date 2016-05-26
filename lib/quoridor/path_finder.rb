c532ebcfad01b36574be32f80381fd716da79560
Angelika Tyborska <atyborska93@gmail.com>
2016-05-26 15:00:11 +0200

    Change Dijkstra's path finding to A*
    Dijkstra's algorithm was painfully slow in this particular case. I need
    to be able to quickly check if there are any paths for 4 pawns on a 9x9
    boards for every possible (max. 128) fence placement, so that's 512 runs
    of the algorithm in a row, and that's just to provide the player with a
    list of all available valid moves. I noticed a over 50x faster
    performance when switching to A*.

--- lib/quoridor/path_finder.rb
+++ lib/quoridor/path_finder.rb
@@ -4,45 +4,64 @@ module Quoridor
       @board = board
     end
 
-    # TODO: MAKE FASTER! Probably change to A*
     def find(start_square, target_squares)
-      distances = {}
-      target_squares.each { |square| distances[square] = 0 }
-      target_squares.each { |square| calculate_adjacent_distances(square, distances) }
-      collect_path(start_square, distances)
-    end
+      open = {}
 
-    def calculate_adjacent_distances(square, distances)
-      new_distance = distances[square] + 1
+      open[start_square] = { parent: :start }
+      fill_costs(start_square, target_squares, open)
 
-      @board.adjacent_squares(square).each do |neighbor|
-        current_distance = distances[neighbor]
+      current_square = pop_min(open)
+      closed = [current_square].to_h
 
-        if current_distance.nil? || current_distance > new_distance
-          unless @board.fence?(square, neighbor)
-            distances[neighbor] = new_distance
-            calculate_adjacent_distances(neighbor, distances)
+      while current_square && !target_squares.include?(current_square[0])
+        @board.adjacent_squares(current_square[0]).each do |square|
+          unless closed[square] || open[square] || @board.fence?(square, current_square[0])
+            open[square]= { parent: current_square[0] }
+            fill_costs(square, target_squares, open)
           end
         end
+
+        current_square = pop_min(open)
+        closed.merge!([current_square].to_h) if current_square
       end
-    end
 
-    def collect_path(target, distances)
-      return nil if distances[target].nil?
+      closed_target_squares = closed.select { |square, costs| target_squares.include?(square) }
+      return nil if closed_target_squares.empty?
 
-      collect_path_partial(target, distances)
+      closest_target_costs_array = closed_target_squares.min_by { |square, costs| costs[:g] }
+
+      parent = closest_target_costs_array[0]
+      path = [parent]
+
+      while (parent = closed[parent][:parent]) && parent != :start
+        path << parent
+      end
+
+      path.reverse[1..-1]
     end
 
     private
 
-    def collect_path_partial(square, distances)
-      return [] if distances[square] == 0
+    def fill_costs(current_square, target_squares, costs)
+      parent = costs[costs[current_square][:parent]]
 
-      @board.adjacent_squares(square).each do |neighbor|
-        if distances[neighbor] && distances[neighbor] == distances[square] - 1 && !@board.fence?(neighbor, square)
-          return [neighbor] + collect_path(neighbor, distances)
-        end
-      end
+      costs[current_square][:h] = heuristic(target_squares, current_square)
+      costs[current_square][:g] = (parent ? parent[:g] : 0) + 1
+      costs[current_square][:f] = costs[current_square][:h] + costs[current_square][:g]
+    end
+
+    def pop_min(open)
+      min = open.min_by { |square, costs| costs[:f] }
+      open.delete(min[0]) if min
+      min
+    end
+
+    def heuristic(target_squares, current_square)
+      target_squares.map { |target| taxicab_metric(target, current_square) }.min
+    end
+
+    def taxicab_metric(a, b)
+      (a[0].ord - b[0].ord).abs + (a[1].ord - b[1].ord).abs
     end
   end
 end
