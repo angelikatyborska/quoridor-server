a0ca1e69ee4495e76115f1e630987b65b5ca0e37
Angelika Tyborska <atyborska93@gmail.com>
2016-05-28 10:10:23 +0200

    Refactor Game#move

--- lib/quoridor/game.rb
+++ lib/quoridor/game.rb
@@ -6,6 +6,7 @@ module Quoridor
   class Game
     attr_reader :turn
 
+    # TODO: add a limit of 10 fences
     def initialize(players)
       unless Quoridor::Rules::Players::ALLOWED_NUMBERS_OF_PLAYERS.include?(players.length)
         fail Quoridor::InvalidNumberOfPlayers.new(players.length)
@@ -15,47 +16,68 @@ module Quoridor
       @board = Quoridor::Board.new
       @players.length.times { |n| @board.add_pawn(Quoridor::Rules::Players::PLAYERS[n][:starting_position]) }
       @turn = 0
+      @winner = nil
     end
 
     def state
       {
         turn: @turn,
+        possible_moves: {
+          fence: Quoridor::Rules::FencePlacement.possible_fence_placements(@board),
+          movements: Quoridor::Rules::Movement.possible_movements(@board, @turn)
+        },
         fences: @board.fences,
         pawns: @board.pawns,
         winner: @winner
       }
     end
 
-    # TODO: refactor
     def move(player, move)
       fail Quoridor::OutOfTurn.new(player) if turn != player
 
-      make_a_move = if move.length == 2
-        if Quoridor::Rules::Movement.correct_movement?(@board, player, move)
-          Proc.new { @board.move_pawn(player, move) }
-        else
-          fail Quoridor::InvalidMovement.new(player, move)
-        end
-      elsif move.length == 3
-        if Quoridor::Rules::FencePlacement.correct_fence_placement?(@board, move)
-          Proc.new { @board.add_fence(move) }
-        else
-          fail Quoridor::InvalidFencePlacement.new(move)
-        end
+      make_a_move = if movement?(move)
+        movement(player, move)
+      elsif fence?(move)
+        fence(player, move)
       else
         fail Quoridor::InvalidMove.new(move)
       end
 
       begin
         make_a_move.call
-        @winner = player if Quoridor::Rules::Winning.has_won?(@board, player)
         increment_turn
+        @winner = player if Quoridor::Rules::Winning.has_won?(@board, player)
+        {winner: @winner}
       rescue Quoridor::Error => e
         fail Quoridor::InvalidMove.new("#{move}: #{e.message}")
       end
     end
 
     private
+
+    def movement(player, move)
+      if Quoridor::Rules::Movement.correct_movement?(@board, player, move)
+        Proc.new { @board.move_pawn(player, move) }
+      else
+        fail Quoridor::InvalidMovement.new(player, move)
+      end
+    end
+
+    def fence(player, move)
+      if Quoridor::Rules::FencePlacement.correct_fence_placement?(@board, move)
+        Proc.new { @board.add_fence(move) }
+      else
+        fail Quoridor::InvalidFencePlacement.new(move)
+      end
+    end
+
+    def fence?(move)
+      move.length == 3
+    end
+
+    def movement?(move)
+      move.length == 2
+    end
 
     def increment_turn
       @turn = (@turn + 1) % @players.count
